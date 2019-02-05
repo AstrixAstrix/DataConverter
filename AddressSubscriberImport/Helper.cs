d548209f63379a61f4a13927f6302a047bd4c32c
kevin jakub <kevin.jakub@newnetservices.net>
2019-02-05 12:18:26 -0700

    addressCoton done

--- AddressSubscriberImport/Helper.cs
+++ AddressSubscriberImport/Helper.cs
@@ -1,12 +1,229 @@
-﻿using System;
+﻿using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using NewNetServices.Module;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NewNetServices.Module.BusinessObjects.CableManagement;
 
 namespace AddressSubscriberImport
 {
-    class Helper
+    public static class Helper
     {
+
+        public static SelectedData GetData(Session session, string table, string[] columns = null)
+        {
+            string sql = $"SELECT {(columns == null ? "*" : string.Join(", ", columns))} from {table}";
+            var result = session.ExecuteQuery(sql);
+            return result;
+        }
+        public static List<Dictionary<string, string>> GetDictionaryListFromData(SelectedData data, string[] columns)
+        {
+            var ret = new List<Dictionary<string, string>>();
+            foreach (var row in data.ResultSet[0].Rows)
+            {
+                Dictionary<string, string> rowDictionary = new Dictionary<string, string>();
+                for (int i = 0; i < row.Values.Length && i < columns.Length; i++)
+                {
+                    rowDictionary[columns[i]] = $"{row.Values[i]}".Trim();
+                }
+                ret.Add(rowDictionary);
+            }
+            return ret;
+        }
+        public static void SetThreadSafeDataLayer(out ThreadSafeDataLayer Tsdl, string conn)
+        {
+            // Code that runs on application startup
+            DevExpress.Xpo.Metadata.XPDictionary dict =
+                new DevExpress.Xpo.Metadata.ReflectionDictionary();
+            dict.GetDataStoreSchema(typeof(NewNetServicesModule).Assembly);
+            DevExpress.Xpo.DB.IDataStore store =
+                DevExpress.Xpo.XpoDefault
+                .GetConnectionProvider(conn,
+                                       DevExpress.Xpo.DB.AutoCreateOption.SchemaAlreadyExists);
+            store = new DevExpress.Xpo.DB.DataCacheNode(new DevExpress.Xpo.DB.DataCacheRoot(store));
+            var layer = new DevExpress.Xpo.ThreadSafeDataLayer(dict, store);
+            Tsdl = layer;
+        }
+        public static int GetSizeForPartition(int collectionSize)
+        {
+            //would like to have 50 or so tasks per collection so
+            int numtasks = 10;
+            while (collectionSize / numtasks < 1)
+                numtasks--;
+            return collectionSize / numtasks;
+        }
+        public static object ProcessList(IEnumerable<Dictionary<string, string>> inlist, string srctable)
+        {
+            foreach (var row in inlist)
+            { //create address, then subscribers    // just need the data from one of the rows since it is the same address for all the subs
+                Guid newAddyGuid = CreateAddress(row, srctable);
+
+                Console.WriteLine($"\n*******\n{System.Threading.Thread.CurrentThread.ManagedThreadId} | {System.Threading.Thread.CurrentThread.IsBackground} | {System.Threading.Thread.CurrentThread.Priority}");
+                using (var uow = new UnitOfWork(Program.Tsdl))
+                {
+                    Guid newSubGuid = CreateSubscriber(uow, row, newAddyGuid, srctable);
+                }
+            }
+            return null;
+
+
+        }
+
+        public static List<string> GetSpecialAddresses(List<Dictionary<string, string>> listdict)
+        {
+            List<string> ret = new List<string>();
+            var distinctAddresses = listdict.Select(x => x["Address_1"] + x["City"] + x["State"]).Distinct();
+            foreach (var VARIABLE in distinctAddresses)
+            {
+                if (listdict.Count(x => x["Address_1"] + x["City"] + x["State"] == VARIABLE) > 1)
+                {
+                    var curent = listdict.Where(x => x["Address_1"] + x["City"] + x["State"] == VARIABLE)
+                        .Select(x => x["Address_1"]).First();
+                    if (!ret.Contains(curent))
+                    {
+                        ret.Add(curent);
+
+                    }
+                }
+            }
+            return ret;
+        }
+
+        /*  
+         *  "Type",
+            "",
+            "",
+            "",
+            " ",
+            " ",
+            " ",
+            "ZIP__4",
+            "Inactive",
+            "LIS",
+            "",
+            "",
+            "",
+            "",
+            "Deployment_Date",
+            "Maximum_Download_Speed",
+            "Maximum_Upload_Speed",
+            "Census_Tract___Block",
+            "SAC",
+            "Tax_Area",
+            "Plant_Area",
+            "Serving_Area",
+            "Service_Area",
+            "Report_Area",
+            "Work_Group",
+            "PSAP",
+            "E911_Community",
+            "E911_County",
+            "Change_By",
+            "Change_Date",
+            ""
+            */
+        static Guid CreateSubscriber(UnitOfWork uow, Dictionary<string, string> subrow, Guid newAddyGuid, string srctable)
+        {
+            Subscriber sub = new Subscriber(uow);
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(subrow["Address_2"]))
+                    sub.LocationName = subrow["Address_2"];
+                else if (!string.IsNullOrWhiteSpace(subrow["Plant_Key"]))
+                    sub.LocationName = subrow["Plant_Key"];
+                else sub.LocationName = subrow["Address_1"];
+                if (uow.Query<LocationStatus>().Any(x => x.StatusName == "ACTIVE"))
+                    sub.Status = uow.Query<LocationStatus>().FirstOrDefault(x => x.StatusName == "ACTIVE");
+                else sub.Status = new LocationStatus(uow) { StatusName = "ACTIVE" };
+                sub.SLID = srctable.First().ToString().ToUpper() + subrow["Addr_ID"];
+                sub.ExternalSystemId = int.TryParse(subrow["Addr_ID"], out int eid) ? eid : 0;
+                sub.SourceType = "MACC IMPORT";
+                sub.SourceTable = srctable;
+                sub.Address = uow.GetObjectByKey<Address>(newAddyGuid);
+                uow.CommitChanges();
+                return sub.Oid;
+            }
+            catch (Exception ex)
+            {
+                NewNetServices.Module.Core.StaticHelperMethods.WriteOut($"Exception in subscriber : {ex}");
+                Console.WriteLine($"Exception in subscriber : {ex}");
+                Console.ReadKey();
+                return Guid.Empty;
+            }
+        }
+
+        public static void DoMultiAddys(List<string> addressesWithMultipleSubs, List<Dictionary<string, string>> listdict, string srctable)
+        {
+            foreach (var addy in addressesWithMultipleSubs)
+            {
+                var subsforaddy = listdict.Where(x => x["Address_1"] + x["City"] + x["State"] == addy);
+
+                //create address, then subscribers    // just need the data from one of the rows since it is the same address for all the subs
+                Guid newAddyGuid = CreateAddress(subsforaddy.ElementAt(0), srctable);
+
+                using (var uow = new UnitOfWork(Program.Tsdl))
+                {
+                    foreach (var subrow in subsforaddy)
+                    {
+                        Guid newSubGuid = CreateSubscriber(uow, subrow, newAddyGuid, srctable);
+                        Console.WriteLine($"*****\nlistdict count {listdict.Count}");
+                        listdict.Remove(subrow);
+                        Console.WriteLine($"listdict count {listdict.Count}\n*****");
+                    }
+                }
+            }
+        }
+        const string subscriberTypeName = "Subscriber";
+        private static Guid CreateAddress(Dictionary<string, string> row, string srctable)
+        {
+            Address ret = null;
+            try
+            {
+                using (var uow = new UnitOfWork(Program.Tsdl))
+                {
+                    ret = new Address(uow);
+                    ret.Street = row["Address_1"];
+                    ret.City = row["City"];
+                    ret.StateProvince = uow.Query<NewNetServices.Module.BusinessObjects.CableManagement.State>()
+                        .FirstOrDefault(x => x.ShortName == row["State"] || x.LongName == row["State"]);
+                    if (ret.StateProvince == null)
+                    {
+                        ret.StateProvince = new State(uow)
+                        {
+                            ShortName = row["State"],
+                            LongName = row["State"],
+                        };
+                    }
+                    ret.ZipPostal = row["ZIP"];//int.TryParse(row["ZIP"], out int zip) ? zip : 0;
+                    ret.ExternalSystemId = int.TryParse(row["Addr_ID"], out int eid) ? eid : 0;
+                    ret.Latitude = float.TryParse(row["Latitude"], out float lat) ? lat : 0f;
+                    ret.Longitude = float.TryParse(row["Longitude"], out float lon) ? lon : 0f;
+                    ret.CensusTract = float.TryParse(row["Census_Tract"], out float ct) ? ct : 0f;
+                    ret.CensusBlock = float.TryParse(row["Census_Block"], out float cb) ? cb : 0f;
+                    ret.SourceType = "MACC IMPORT";
+                    ret.SourceTable = srctable;
+                    uow.CommitChanges();
+                }
+                return ret.Oid;
+            }
+            catch (Exception ex)
+            {
+                NewNetServices.Module.Core.StaticHelperMethods.WriteOut($"Exception in adfdress : {ex}");
+                Console.WriteLine($"Exception in adfdress : {ex}");
+                Console.ReadKey();
+                return Guid.Empty;
+            }
+        }
     }
 }
--- AddressSubscriberImport/Program.cs
+++ AddressSubscriberImport/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +11,141 @@ namespace AddressSubscriberImport
 {
     class Program
     {
+        static string[] columns =
+        {
+            "Type",
+            "Address_1",
+            "Address_2",
+            "Plant_Key",
+            "City",
+            "State",
+            "ZIP",
+            "ZIP__4",
+            "Inactive",
+            "LIS",
+            "Latitude",
+            "Longitude",
+            "Census_Tract",
+            "Census_Block",
+            "Deployment_Date",
+            "Maximum_Download_Speed",
+            "Maximum_Upload_Speed",
+            "Census_Tract___Block",
+            "SAC",
+            "Tax_Area",
+            "Plant_Area",
+            "Serving_Area",
+            "Service_Area",
+            "Report_Area",
+            "Work_Group",
+            "PSAP",
+            "E911_Community",
+            "E911_County",
+            "Change_By",
+            "Change_Date",
+            "Addr_ID"
+        };
+        static string connectionString;
+
+        public static ThreadSafeDataLayer Tsdl;
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting..");
+            SelectedData result = null;
+            Helper.SetThreadSafeDataLayer(out Tsdl, ConnectionString);
+            var t = Task.Factory
+                .StartNew(async () =>
+            {
+                var tasks = new List<Task>();
+                using (UnitOfWork uow = new UnitOfWork(Tsdl))
+                {
+                    Console.WriteLine("Getting Data..COLTON");
+                    result = Helper.GetData(uow, "AddressData", columns);
+                }
+                if (result != null)
+                {
+                    var listdict = Helper.GetDictionaryListFromData(result, columns);
+                    Console.WriteLine("OK");
+                    Console.WriteLine("Getting list of addresses with multiple...");
+                    List<string> addressesWithMultipleSubs = Helper.GetSpecialAddresses(listdict);
+                    addressesWithMultipleSubs.ForEach((x) => Console.WriteLine(x));
+                    Console.WriteLine("OK");
+                    Console.WriteLine("Handling addresses with multiple subs...");
+                    Helper.DoMultiAddys(addressesWithMultipleSubs, listdict, "COLTON");
+                    Console.WriteLine("OK");
+                    Console.WriteLine("Partitioning...");
+                    var partsList = new List<List<Dictionary<string, string>>>();
+                    listdict.Partition(Helper.GetSizeForPartition(listdict.Count))
+                        .ForEach((x) => partsList.Add(x.ToList()));
+                    Console.WriteLine($"{partsList.Count} parts of {partsList[0].Count} records.\n Beginning tasks...");
+                    //ick of tasks in parallel
+                    int i = 1;
+                    Parallel.ForEach(partsList,
+                                     (inlist) =>
+                    {
+                        Console.WriteLine($"Start Processing chunk { i}");
+                        tasks.Add(Task.Factory.StartNew(() => Helper.ProcessList(inlist, "COLTON")));
+                        Console.WriteLine($"End Processing chunk {i++}");
+                    });
+                    Console.ReadKey();
+                    await Task.WhenAll(tasks.ToArray());
+
+                    Console.WriteLine("Finished importing COLTON");
+                    Console.WriteLine("Begin MONITOR");
+                    using (UnitOfWork uow = new UnitOfWork(Tsdl))
+                    {
+                        Console.WriteLine("Getting Data..MONITOR");
+                        result = Helper.GetData(uow, "AddressData2", columns);
+                    }
+                    if (result != null)
+                    {
+                        listdict = Helper.GetDictionaryListFromData(result, columns);
+                        Console.WriteLine("OK");
+                        Console.WriteLine("Getting list of addresses with multiple...");
+                        addressesWithMultipleSubs = Helper.GetSpecialAddresses(listdict);
+                        addressesWithMultipleSubs.ForEach((x) => Console.WriteLine(x));
+                        Console.WriteLine("OK");
+                        Console.WriteLine("Handling addresses with multiple subs...");
+                        Helper.DoMultiAddys(addressesWithMultipleSubs, listdict, "MONITOR");
+                        Console.WriteLine("OK");
+                        Console.WriteLine("Partitioning...");
+                        partsList = new List<List<Dictionary<string, string>>>();
+                        listdict.Partition(Helper.GetSizeForPartition(listdict.Count))
+                            .ForEach((x) => partsList.Add(x.ToList()));
+                        Console.WriteLine($"{partsList.Count} parts of {partsList[0].Count} records.\n Beginning tasks...");
+                        //ick of tasks in parallel
+                        i = 1;
+                        Parallel.ForEach(partsList,
+                                         (inlist) =>
+                        {
+                            Console.WriteLine($"Start Processing chunk { i}");
+                            tasks.Add(Task.Factory.StartNew(() => Helper.ProcessList(inlist, "MONITOR")));
+                            Console.WriteLine($"End Processing chunk {i++}");
+                        });
+                        Console.ReadKey();
+                        await Task.WhenAll(tasks.ToArray());
+
+                        Console.WriteLine("Finished importing MONITOR");
+                    }
+                }
+            });//end outer task
+               // t.Wait();
 
             Console.ReadKey();
+        }
+
+        public static string ConnectionString
+        {
+            get
+            {
+                if (connectionString != null) return connectionString;
+                else
+                {
+                    connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                    return connectionString;
+                }
+            }
+            set { connectionString = value; }
         }
     }
 }
